39c6edd989603c1831334a91f8cccdd365cebb99
leocsas <leocristian2014@gmail.com>
2025-08-20 21:30:11 -0300

    Criando primeiro projeto  NODE

---
+++ Praticas/01-primeiro-projeto/index.js
@@ -0,0 +1,18 @@
+console.log("Rodando meu primeiro projeto em NODE")
+
+let nome = "Leonardo"
+let sobrenome = "Soares"
+
+console.log(nome + " " + sobrenome)
+
+let idade = 23
+
+console.log("A idade de " + nome + " é " + idade + ".")
+
+let exerciciosNota = 0.5
+let trabalhoNota = 1.5
+let provaNota = 4.5
+
+let NotaCalculada = exerciciosNota + trabalhoNota + provaNota
+
+console.log("NOTA A1: " + NotaCalculada + ".")
