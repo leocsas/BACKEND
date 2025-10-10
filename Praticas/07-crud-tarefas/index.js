58c9fe6499e5904a8dcae67d2f946eeb35b0d6fa
leocsas <leocristian2014@gmail.com>
2025-10-09 22:52:24 -0300

    Mudança

--- Praticas/07-crud-tarefas/index.js
+++ Praticas/07-crud-tarefas/index.js
@@ -6,14 +6,3 @@ const app = express()
 app.use(express.json())
 
 // Conectar ao banco Mongo
-mongoose.connect('mongodb+srv://leocsas:x858u0qv8ceLyO6R@cluster0.qvdihfb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-.then(() => {
-    console.log('Conectado ao MongoDB')
-})
-.catch(err => {
-    console.log('Erro ao conectar no banco MongoDB.', err)
-})
-
-app.listen(3000, () => {
-    console.log('Aplicação rodando em http://localhost:3000')
-})
